e2eccc607e34425036130877dc49610f33293c00
Przemek B <pprzemek89@gmail.com>
2024-02-15 14:53:13 +0100

    08.02

---
+++ aplikacjaBlog/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+
+    STATUS_COICES = {
+        ("draft", "draft"),
+        ("published", "Published")
+    }
+
+    status = models.CharField(max_length=10, choices=STATUS_COICES, default="draft")
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
