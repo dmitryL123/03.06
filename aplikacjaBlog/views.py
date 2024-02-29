cf9dfd9461003ad06b11d8ae46a5c85b0514d187
Przemek B <pprzemek89@gmail.com>
2024-02-29 14:59:37 +0100

    29.02

--- aplikacjaBlog/views.py
+++ aplikacjaBlog/views.py
@@ -1,8 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from aplikacjaBlog.models import Post
 
-
 def post_list(request):
     posts = Post.objects.all()
-    return "test 123"
+    return render(request, 'Blog/post/list.html')
