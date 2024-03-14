40174aa2d7f302a1ead3e03e101752ae540c5b88
Przemek B <pprzemek89@gmail.com>
2024-03-14 15:02:12 +0100

    14.03

--- aplikacjaBlog/views.py
+++ aplikacjaBlog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from aplikacjaBlog.models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'Blog/post/list.html')
+    posts = Post.objects.all().filter(status='published')
+    return render(request, 'Blog/post/list.html', {"posts": posts})
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
+    return  render(request, 'blog/post/detail.html', {"post": post })
