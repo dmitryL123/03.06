26e8d28146ad4e04eee40a36393d7aa0889e9f0e
skiparb <skiparb@gmail.com>
2024-04-11 14:59:13 +0200

    11.04

--- aplikacjaBlog/admin.py
+++ aplikacjaBlog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from aplikacjaBlog.models import Post
+from aplikacjaBlog.models import Post, Comment
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -11,3 +12,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'name', 'body')
--- aplikacjaBlog/views.py
+++ aplikacjaBlog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from aplikacjaBlog.forms import CommentForm
 from aplikacjaBlog.models import Post
 
 #def post_list(request):
@@ -15,5 +16,22 @@ class PostListView(ListView):
     template_name = 'Blog/post/list.html'
 
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return  render(request, 'blog/post/detail.html', {"post": post })
+    post = get_object_or_404(Post, slug=slug,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    comment_form = CommentForm()
+
+    return render(request, 'Blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+
